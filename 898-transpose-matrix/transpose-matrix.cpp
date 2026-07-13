7ec8d0221c5c0e46ac04f1c5fd05939eac17b6dd
Anshuman <150334667+Anshumank229@users.noreply.github.com>
2026-07-13 13:40:36 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (34.30%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        
+        // Transpose matrix has size cols x rows
+        vector<vector<int>> t(cols, vector<int>(rows));
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                t[j][i] = matrix[i][j];
+            }
+        }
+        return t;
+    }
+};
